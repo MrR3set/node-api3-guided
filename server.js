8beecd44398c393c194ca13ad1ea705105a0271b
MrR3set <bairon.pazrobles@gmail.com>
2020-04-08 10:42:31 -0700

    asdd

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express'); // importing a CommonJS module
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+server.use(helmet());
+server.use(morgan("short"));
+server.use(logger);
+server.use(auth);
 server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
-
   res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
@@ -18,3 +23,18 @@ server.get('/', (req, res) => {
 });
 
 module.exports = server;
+
+function logger(req,res,next){
+  console.log(`${req.method} Req to : ${req.originalUrl}`);
+  next();
+}
+
+function auth(req,res,next){
+  if(req.query.pass==="mellon"){
+    console.log("Allowed");
+    next();
+  }else{
+    res.status(403).json({message:"Not allowed"});
+  }
+
+}
